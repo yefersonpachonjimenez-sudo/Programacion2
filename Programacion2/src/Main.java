c8ebd029ea3b356ceeb481e9a57e5ee88e548b11
Yefer <yefersonpachonjimenez@gmail.com>
2026-02-13 20:56:15 -0500

    salida consola

--- Programacion2/src/Main.java
+++ Programacion2/src/Main.java
@@ -1,7 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-
+ System.out.println("hola mundo");
+ System.out.println("REPASO COMANDO GIF"):
 
 
 
